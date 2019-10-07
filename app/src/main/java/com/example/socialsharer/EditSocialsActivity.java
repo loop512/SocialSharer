ae0e4e31bd031aa0c9ba818ed3aab5b211c274ce
davtc <47212365+davtc@users.noreply.github.com>
2019-10-07 20:02:16 +1100

    Updated profile fragment

---
+++ app/src/main/java/com/example/socialsharer/EditSocialsActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialsharer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditSocialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_socials);
+    }
+}
--- app/src/main/java/com/example/socialsharer/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/socialsharer/Fragments/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -62,7 +63,9 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate (R.layout.fragment_profile, container, false);
+        ImageView profileImage = (ImageView)view.findViewById(R.id.profile_image);
+        profileImage.setImageResource(R.drawable.unknown);
+        return view;
     }
 }
