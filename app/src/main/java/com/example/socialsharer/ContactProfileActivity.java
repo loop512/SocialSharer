a1607bab6b5a8e5a1c56115053bfffb57d0362bf
loop512 <1695420@qq.com>
2019-10-20 06:49:44 +1100

    add back to previous function an contects activity, notice that in firebase | 1: send request to that user | 2: rejected request by that user | 3 confirmed request by that user | 4 received friend request from that user |

--- app/src/main/java/com/example/socialsharer/ContactProfileActivity.java
+++ app/src/main/java/com/example/socialsharer/ContactProfileActivity.java
@@ -1,14 +1,28 @@
 package com.example.socialsharer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_profile);
+        final Button backButton = findViewById(R.id.backButton_contact_activity);
+
+        // Set go back buttom
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.popBackStack();
+                finish();
+            }
+        });
+
     }
 }
